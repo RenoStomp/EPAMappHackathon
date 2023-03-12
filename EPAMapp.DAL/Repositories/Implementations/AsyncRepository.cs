eb9dc87f44ce9b83676543ae8587562ac91fd1fb
Emil Abdullaev <renoemilll@gmail.com>
2023-03-13 03:27:16 +0600

    Add: IAsyncRepository interface added; AsyncRepository repository added; both filled

---
+++ EPAMapp.DAL/Repositories/Implementations/AsyncRepository.cs
@@ -0,0 +1,55 @@
+﻿using EPAMapp.DAL.Repositories.Interfaces;
+using EPAMapp.DAL.SqlServer;
+using EPAMapp.Domain.Models.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace EPAMapp.DAL.Repositories.Implementations
+{
+    public class AsyncRepository<T> : IAsyncRepository<T>
+        where T : BaseEntity
+    {
+        private readonly AppDbContext _context;
+        public AsyncRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(T entity)
+        {
+            if (DataBaseIsNotExist()) return;
+
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<T> Get()
+        {
+            if(DataBaseIsNotExist()) return default(IQueryable<T>);
+
+            return _context.Set<T>();
+        }
+
+        public async Task Update(T entity)
+        {
+            if (DataBaseIsNotExist()) return;
+
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+            if (DataBaseIsNotExist()) return;
+
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+        public bool DataBaseIsNotExist()
+        {
+            if (_context.Set<T>() == default(DbSet<T>)) 
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ EPAMapp.DAL/Repositories/Interfaces/IAsyncRepository.cs
@@ -0,0 +1,13 @@
+﻿using EPAMapp.Domain.Models.Common;
+
+namespace EPAMapp.DAL.Repositories.Interfaces;
+
+public interface IAsyncRepository<T>
+    where T : BaseEntity
+{
+    public Task Create(T entity);        // C - Create
+    public IQueryable<T> Get();        // R - Read
+    public Task Update(T Item);     // U - Update
+    public Task Delete(T item);     // D - Delete
+
+}
