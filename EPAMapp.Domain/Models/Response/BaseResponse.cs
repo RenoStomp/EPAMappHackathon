655c529ad71b22ce3e175a9378ff87ab1374383b
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-03-13 00:31:57 +0600

    Add: Response class added

---
+++ EPAMapp.Domain/Models/Response/BaseResponse.cs
@@ -0,0 +1,12 @@
+﻿
+using EPAMapp.Domain.Models.Interfaces;
+
+namespace EPAMapp.Domain.Models.Response
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+        public string Description { get; set; }
+
+        public T Data { get; set; }
+    }
+}
