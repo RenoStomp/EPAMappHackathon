3581f9599926824483f7bbed89102b2e33946b65
Emil Abdullaev <renoemilll@gmail.com>
2023-03-13 22:47:25 +0600

    Add: DTO's added

---
+++ EPAMapp.Services/DTO/Update/UpdateAdmin.cs
@@ -0,0 +1,10 @@
+﻿namespace EPAMapp.Services.DTO.Update
+{
+    public class UpdateAdmin
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string NickName { get; set; }
+    }
+}
---
+++ EPAMapp.Services/DTO/Update/UpdateNote.cs
@@ -0,0 +1,10 @@
+﻿namespace EPAMapp.Services.DTO.Update
+{
+    public class UpdateNote
+    {
+        public int Id { get; set; }
+        public string Report { get; set; }
+
+
+    }
+}
