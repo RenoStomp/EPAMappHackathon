1c6dbc6687d6d8cee828daa42fe53f459a146459
Emil Abdullaev <renoemilll@gmail.com>
2023-03-14 02:48:06 +0600

    TEST! Fix: Update totally changed; automappers added; Update folder and all classes inside deleted issue: one type check is missing

--- EPAMapp.Services/Interfaces/IAsyncBaseService.cs
+++ EPAMapp.Services/Interfaces/IAsyncBaseService.cs
@@ -8,7 +8,7 @@ namespace EPAMapp.Services.Interfaces
        public Task<IBaseResponse<T>> Create(T model);       //C - Create
        public IBaseResponse<List<T>> GetAll();              //R - Read All
        public IBaseResponse<T> GetModelById(int id);             //R - Read One
-       public Task<IBaseResponse<T>> Update(T model);       //U - Update
+       public Task<IBaseResponse<T>> Update(T model);       //U - Map
        public Task<IBaseResponse<T>> Delete( T model);      //D - Delete
 
     }
---
+++ EPAMapp.Services/Mapping/Mapper.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using EPAMapp.Domain.Models.Common;
+using EPAMapp.Services.DTO.Update;
+using EPAMapp.Services.Mapping;
+
+namespace EPAMapp.Services.Update
+{
+    public class Mapper<T, TModel>
+        where T : BaseEntity
+        where TModel : BaseEntity
+    {
+        private readonly IMapper _mapper;
+
+        public Mapper()
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            _mapper = mappingConfig.CreateMapper();
+        }
+
+        public T? Map(TModel model)
+        {
+            return model switch
+            {
+                DTOUpdateUser userModel => MapperHelper.MapDtoUserToUser(userModel, _mapper) as T,
+                DTOUpdateAdmin adminModel => MapperHelper.MapDtoAdminToAdmin(adminModel, _mapper) as T,
+                DTOUpdateNote noteModel => MapperHelper.MapDtoNoteToNote(noteModel, _mapper) as T,
+                _ => default(T)
+            };
+        }
+
+    }
+}
