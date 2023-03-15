af5a8e929dff6c1f126e1137ee22203f91171adf
ayubarr <114123666+ayubarr@users.noreply.github.com>
2023-03-15 22:50:45 +0600

    Add: SOLT ADDED

--- EPAMapp.Domain/Helpers/HashPasswordHelper.cs
+++ EPAMapp.Domain/Helpers/HashPasswordHelper.cs
@@ -5,13 +5,14 @@ namespace EPAMapp.Domain.Helpers
 {
     public static class HashPasswordHelper
     {
+        private static string Solt = "PowerOfTheSolt";
         public static string HashPassword(string password)
         {
             using (var sha256 = SHA256.Create())
             {
-                var hashhedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var hashhedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password + Solt));
                 var hash = BitConverter.ToString(hashhedBytes).Replace("-", "").ToLower();
-
+                
                 return hash;
             }
         }
